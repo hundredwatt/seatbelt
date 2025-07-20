1e1dff3ee11c73229268f3218d3de9f613733309
Jason Nochlin <hundredwatt@users.noreply.github.com>
2025-07-19 19:34:32 -0600

    x

--- seatbelt/pkg/clickhouse/clickhouse_target.go
+++ seatbelt/pkg/clickhouse/clickhouse_target.go
@@ -94,7 +94,7 @@ func (t *ClickHouseTarget) Scan(ctx context.Context, table seatbelt.Table) (*sea
 		SELECT 
 			%s AS pk,
 			xxh3(%s) AS target_hash
-		FROM %s
+		FROM %s FINAL
 		WHERE _peerdb_is_deleted = 0
 	`, table.PrimaryKey(), table.SQLTextExpressionForTargetHashing(), table.TargetName())
 
